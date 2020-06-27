f3d34be0d58dd945f07f2d3ffb90a99d45d39bd8
Baskaran Ashwin <ashwin.baskaran@wallstreetenglish.com>
2020-06-27 18:12:58 +0530

    removed dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoConnect = require('./utils/mongoConnect');
-const serverless = require('serverless-http');
 const config = require('./config/dev');
 /** Routes */
 const authRoutes = require('./routes/authRoutes');
